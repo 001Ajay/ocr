db12daa091f7aa3f32ead0d74686a7f7d7a9214d
001Ajay <ajaydas5478@gmail.com>
2017-08-07 21:10:31 +0530

    stable update

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -233,8 +233,8 @@
 		//$('#start-over').trigger('click');
 		
         //show the result
-        $('blockquote p').html('&bdquo;' + resultText + '&ldquo;');
-        $('blockquote footer').text('(' + resultText.length + ' characters)')
+        //$('blockquote p').html('&bdquo;' + resultText + '&ldquo;');
+        //$('blockquote footer').text('(' + resultText.length + ' characters)')
     }
 
     /*********************************
@@ -308,7 +308,7 @@
 
     $('#adjust').click(function () {
         step3();
-        changeStep(3);
+        //changeStep(3);
     });
 
     $('#go-back').click(function () {
@@ -328,75 +328,26 @@
         return false;
     });
 	
-	$.fn.floatLabels = function (options) {
-
-        // Settings
-        var self = this;
-        var settings = $.extend({}, options);
-
-
-        // Event Handlers
-        function registerEventHandlers() {
-            self.on('input keyup change', 'input, textarea', function () {
-                actions.swapLabels(this);
-            });
-        }
-
-
-        // Actions
-        var actions = {
-            initialize: function() {
-                self.each(function () {
-                    var $this = $(this);
-                    var $label = $this.children('label');
-                    var $field = $this.find('input,textarea').first();
-
-                    if ($this.children().first().is('label')) {
-                        $this.children().first().remove();
-                        $this.append($label);
-                    }
-
-                    var placeholderText = ($field.attr('placeholder') && $field.attr('placeholder') != $label.text()) ? $field.attr('placeholder') : $label.text();
-
-                    $label.data('placeholder-text', placeholderText);
-                    $label.data('original-text', $label.text());
-
-                    if ($field.val() == '') {
-                        $field.addClass('empty')
-                    }
-                });
-            },
-            swapLabels: function (field) {
-                var $field = $(field);
-                var $label = $(field).siblings('label').first();
-                var isEmpty = Boolean($field.val());
-
-                if (isEmpty) {
-                    $field.removeClass('empty');
-                    $label.text($label.data('original-text'));
-                }
-                else {
-                    $field.addClass('empty');
-                    $label.text($label.data('placeholder-text'));
-                }
-            }
-        }
-
-
-        // Initialization
-        function init() {
-            registerEventHandlers();
-
-            actions.initialize();
-            self.each(function () {
-                actions.swapLabels($(this).find('input,textarea').first());
-            });
-        }
-        init();
+	$('#submitBtn').click(function(){
+		var name = $('#name').val();
+		var country = $('#country').val();
+		var ssn = $('#ssn').val();
+		var info = {
+			Name:name,
+			Country:country,
+			SSN:ssn
+		};
 		
-		return this;
-	};
+		$.ajax({
+			type: "POST",
+			url: "https://pegabpmhyd.cognizant.com/prweb/PRRestService/Onboarding/Profile/ProfileSetUp",
+			data: info,
+			success: function(data){
+				console.log(data);
+			},
+			dataType: "JSON"
+		});		
+	});	
 	
-	 $('.float-label-control').floatLabels();
-        
+	        	
 })();
